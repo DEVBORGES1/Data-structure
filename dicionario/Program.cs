db2a6fbe304eb84198d118af82abe2d8964e7c5b
João Vítor Pereira da Silva <403392@unoesc.lan>
2024-09-25 20:58:44 -0300

    aulam

---
+++ dicionario/Program.cs
@@ -0,0 +1,45 @@
+﻿// Diferente da TabelaHash, o Dictionary nos permite 
+// especificar o tipo de dado que será utilizado nos pares
+// key -> value
+// Isso nos fornece uma estrutura fortemente tipada 
+
+Dictionary<string, string> dictionary = 
+    new Dictionary<string, string>()
+{
+    { "Chave 1", "Valor 1" },
+    { "Chave 2", "Valor 2" },
+};
+
+// OBTER O VALOR DO DICTIONARY
+string val = dictionary["Chave 1"];
+
+try
+{
+    val = dictionary["Chave 3"];
+}
+catch (KeyNotFoundException ex) // Captura a exceção específica
+{
+    Console.WriteLine("Erro não tem essa porra da chave 3.");
+    Console.WriteLine(ex.Message);
+}
+
+if (dictionary.ContainsKey("Chave 2"))
+{
+    Console.WriteLine($"Chave 2: {dictionary["Chave 2"]}");
+}
+
+if (dictionary.TryGetValue("Chave 4", out string? valor))
+{
+    Console.WriteLine($"Chave 4: {valor}");
+}
+else
+{
+    Console.WriteLine("Chave 4 não encontrada nesse caralho seu porra para de ser burro leproso.");
+}
+
+dictionary["Chave 0"] = "Valor 0";
+
+foreach (KeyValuePair<string, string> kvp in dictionary)
+{
+    Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+}
---
+++ tabela hash/Program.cs
@@ -0,0 +1,45 @@
+﻿// Exemplo de uma agenda telefônica 
+
+using System;
+using System.Collections;
+
+Hashtable phonebook = new Hashtable()
+{
+    { "LUIS", "49-99990-2488" },
+    { "MAURICIO GONZATTO", "49-99975-8575" },
+    { "Gabriel Bianchi", "49-99105-8904" }
+};
+
+// É possível adicionar elementos de diversas formas 
+phonebook["FABIANO"] = "49-99176-8255";
+
+// Ou pelo método Add()
+phonebook.Add("Marcos Henrique", "49-99202-6169");
+
+// Entretanto, a tabela hash verifica se há 
+// duplicidade de chave e pode lançar uma 
+// exceção 
+
+try
+{
+    phonebook.Add("MAURICIO GONZATTO", "49-99975-8575");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Deu pau aqui, viado, repetiu o valor demais");
+    Console.WriteLine(ex.Message);
+    
+}
+
+Console.WriteLine("Agenda telefônica.");
+if (phonebook.Count == 0)
+{
+    Console.WriteLine("A agenda está vazia.");
+}
+else
+{
+    foreach (DictionaryEntry entry in phonebook)
+    {
+        Console.WriteLine($"{entry.Key}: {entry.Value}");
+    }
+}
