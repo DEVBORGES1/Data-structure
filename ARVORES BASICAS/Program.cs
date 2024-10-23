9c6335f328e0260932e27f3859050e2453041452
João Vitor <joaovitorpereira.10112@gmail.com>
2024-10-23 20:20:44 -0300

    Arvore

--- ARVORES BASICAS/Program.cs
+++ ARVORES BASICAS/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ARVORES_BASICAS;
+
+Tree<int> tree = new Tree<int>();
+tree.Root = new Node<int> { Data = 100 };
+tree.Root.Children = new List<Node<int>> { 
+    new Node<int> { Data = 50 }, 
+    new Node<int> { Data = 1 }, 
+    new Node<int> { Data = 150 } 
+};
+
+Node<int> node12 = new Node<int> { Data = 12 };
+Node<int> node45 = new Node<int> { Data = 45 };
+Node<int> node21 = new Node<int> { Data = 21 };
+
+
+tree.Root.Children[0].Children!.Add (node12);
